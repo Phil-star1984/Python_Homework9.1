23040785b986328cc0d2ecdf5cf195955c45279b
Philipp Mulfinger <info@phil-splash.de>
2020-11-03 21:19:10 +0100

    First commit

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print()
+print("Hi there, my name is Alexis and I was created to help you convert kilometers into miles.")
+
+while True:
+    route = int(input("Enter kilometers: "))
+
+    print("This corresponds to " + str(int(route) / 1.61) + " miles.")
+
+    another_conversion = input("Do you want to do another conversion? (yes/no) ")
+
+    if another_conversion == "no":
+        print("Ok, that's it for today. Have a good night.")
+        break
+    else:
+        print("Great, let's do another one!")
+
